48e2cbbc19a8853b0014fb5b0276e9a5e59ed1dc
emilievinge <emiliev@hiof.no>
2026-02-27 14:04:06 +0100

    Start på ak3. Bruker API :rocket:

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+export default function Home(){
+    const [search, setSearch] = useState()
+    
+    const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+    // IKKE GJØR DETTE!!! API key skal ikke ligge "offentlig".
+    const apiKey = 'bb8b2f65'
+
+    const getMovies = async()=>{
+        try
+        {
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+            console.log(data)
+
+        }
+        catch(err)
+        {
+            console.error(err);
+        }
+    }
+
+    const handleChange = (e)=>{
+        setSearch(e.target.value)
+    }
+
+    return (
+        <main>
+            <h1>Forside</h1>
+            <form>
+                <label>
+                    Søk etter film
+                    <input  type="search" placeholder="Harry Potter" onChange={handleChange}></input>
+                </label>
+            </form>
+            <button onClick={getMovies}>Søk</button>
+        </main>
+    )
+}
